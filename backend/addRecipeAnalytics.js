959bf069d9c83a0490cddaea3fbb74ccf913e30c
robinskumar73 <robinskumar73@gmail.com>
2016-02-28 06:43:12 +0530

    first commit

---
+++ backend/addRecipeAnalytics.js
@@ -0,0 +1,155 @@
+'use strict';
+var init = function(server, databaseObj, helper, packageObj) {
+    //When a recipe is created add a analytics with recipe..
+    databaseObj.Recipe.observe('after save', function(ctx, next) {
+        if (ctx.isNewInstance) {
+            var RecipeAnalytic = databaseObj.RecipeAnalytic;
+            var data = {
+                totalViews: 0,
+                averageRating: 0,
+                totalComment: 0
+            };
+            if (ctx.instance) {
+                //Create a RecipeAnalytics..
+                ctx.instance.recipeAnalytics.create(data, function(err, analyticsObj) {
+                    if (err) {
+                        console.error(err);
+                        return false;
+                    }
+                    //Analytics successfully added to recipe..
+                    //Add analytic for two way communication..
+                    ctx.instance.recipeAnalyticId = analyticsObj.id;
+                    //Resave it..
+                    ctx.instance.save({}, function(err, val){
+                        if(err){
+                            console.error("Data cannot be saved..");
+                            return false;
+                        }
+                    });
+                });
+            }
+            next();
+        } else {
+            next();
+        }
+    });
+
+
+    //When a RecipeIngredients is fetched increase the views. in recipe.
+    databaseObj.RecipeIngredients.observe('loaded', function(ctx, next) {
+        if (ctx.instance === undefined) {
+            //console.error("Error recipe data is not present");
+            return next();
+        }
+    /*    var loopback = helper.getLoopbackObj();
+        //Check is user is Admin..
+        var login = helper.loadPlugin('login');
+        var currentContext = loopback.getCurrentContext();
+        //first check is the user is not admin..
+        login.isAdmin(server, currentContext, function(err, value){
+            console.log(err);
+            console.log(value);
+        });*/
+
+        var RecipeId = ctx.instance.recipeId;
+        //Now increase a recipe views..
+        databaseObj.RecipeAnalytic.find({
+            where: {
+                recipeId: RecipeId
+            }
+        }, function(err, recipeAnalyticObj) {
+            if (err) {
+                console.log("Error fetching recipe analytic data.");
+                console.error(err);
+                next();
+            } else {
+                if(recipeAnalyticObj.length){
+                    recipeAnalyticObj = recipeAnalyticObj[0];
+                }else{
+                    console.error("No recipe analytics data model present");
+                    return next();
+                }
+                recipeAnalyticObj.totalViews = parseInt(recipeAnalyticObj.totalViews) + 1;
+                //Now save the ingredients..
+                recipeAnalyticObj.save({}, function(err, object) {
+                    if (err) {
+                        console.log("Error fetching recipe ingredients data.");
+                        console.error(err);
+
+                    }
+                    //Successfully increased views..
+
+                });
+                next();
+            }
+        });
+    }); //loaded
+
+
+    //When a comment is created increment ratings and comment value..of analytics..
+    databaseObj.Comments.observe('after save', function(ctx, next) {
+        if (ctx.isNewInstance) {
+            if(ctx.instance){
+                //Now increment comment and ratings value..
+                databaseObj.RecipeAnalytic.find({
+                    where:{
+                        recipeId: ctx.instance.recipeId
+                    }
+                }, function(err, recipeAnalyticObj){
+
+                    if(err){
+                        console.error("Error finding recipeIngredients data from database..");
+                        next();
+                        return false;
+                    }
+
+                    if(recipeAnalyticObj.length){
+                        recipeAnalyticObj = recipeAnalyticObj[0];
+                    }else{
+                        console.error("No recipe analytics data model present");
+                        return next();
+                    }
+
+                    //Now calculate the average ratings..
+                    var totalRating = (parseInt(recipeAnalyticObj.totalComment) * parseInt(recipeAnalyticObj.averageRating) )
+                    //now increment comment..
+                    recipeAnalyticObj.totalComment  =  parseInt(recipeAnalyticObj.totalComment) + 1;
+                    //Now add this comment rating..
+                    if(ctx.instance.rating !== undefined){
+                        totalRating = totalRating + parseInt(ctx.instance.rating);
+                        //Now calculate average. rating..
+                        var avgRating = totalRating / recipeAnalyticObj.totalComment;
+                        recipeAnalyticObj.averageRating = avgRating;
+                    }//if
+
+                    //Now save the data..
+                    recipeAnalyticObj.save({}, function(err, obj){
+                        if(err){
+                            console.error("Error in Recipe analytics total comment and rating incrementing..");
+
+                        }else{
+                            //done incrementing value..
+                            //
+                        }
+                    });
+
+                    //Now call the next middleware..
+                    next();
+                });// find RecipeAnalytic
+
+            }else{
+                next();
+            }
+        }else{
+            next();
+        }
+    });
+
+
+}
+
+
+
+module.exports = {
+    init: init
+}
